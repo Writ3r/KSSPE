a511f2c6224c972f748a787e8e746a9d38afdee3
Lucas Wing <lucas.wing@aol.com>
2019-05-08 15:42:00 -0400

    da faq

---
+++ KSSPE/controller/ListOverDueInventoryTransaction.java
@@ -0,0 +1,111 @@
+// specify the package
+package controller;
+
+// system imports
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import java.util.Properties;
+import java.util.Vector;
+import java.util.Enumeration;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+// project imports
+import event.Event;
+import exception.InvalidPrimaryKeyException;
+import exception.MultiplePrimaryKeysException;
+
+import userinterface.View;
+import userinterface.ViewFactory;
+import model.CheckOut;
+import model.CheckOutCollection;
+
+/** The class containing the ListOverDueInventoryTransaction for the KSSPE application */
+//==============================================================
+public class ListOverDueInventoryTransaction extends Transaction
+{
+	private String errorMessage = "";
+	private Receptionist myReceptionist;
+	private CheckOut myCheckOut;
+	private CheckOutCollection myCheckOutList;
+
+
+	//------------------------------------------------------------
+	public ListOverDueInventoryTransaction() throws Exception
+	{
+		super();
+		processTransaction();
+	}
+
+	//------------------------------------------------------------
+	public void processTransaction()
+	{
+		myCheckOutList = new CheckOutCollection();
+		myCheckOutList.findOverDue();
+		
+		try
+		{	
+			Scene newScene = createView();	
+			swapToView(newScene);
+		}
+		catch (Exception ex)
+		{
+			new Event(Event.getLeafLevelClassName(this), "processTransaction",
+					"Error in creating EquipmentCollectionView", Event.ERROR);
+		}
+	}
+	
+	//-----------------------------------------------------------
+	public Object getState(String key)
+	{
+		if (key.equals("Error") == true)
+		{
+			return errorMessage;
+		}
+		else if (key.equals("Alert") == true)
+		{
+			return true;
+		}
+		else if (key.equals("CheckOutList") == true)
+		{
+			return myCheckOutList;
+		}
+		else
+			return null;
+	}
+
+	//-----------------------------------------------------------------
+	public void stateChangeRequest(String key, Object value)
+	{
+		errorMessage = "";
+		
+		if (key.equals("DoYourJob") == true)
+		{
+			myReceptionist = (Receptionist)value;
+			doYourJob();
+		}
+		else if(key.equals("clearState") == true)
+		{
+			myCheckOut = null;
+		}
+		else if (key.equals("CancelCheckOutList") == true)
+		{
+			myReceptionist.stateChangeRequest("CancelTransaction", null);
+		}
+		
+		setChanged();
+        notifyObservers(errorMessage);
+	}
+
+	//------------------------------------------------------
+	protected Scene createView()
+	{
+		Scene currentScene;
+
+		View newView = ViewFactory.createView("CheckOutCollectionView", this);
+		currentScene = new Scene(newView);
+
+		return currentScene;
+	}
+}
+
