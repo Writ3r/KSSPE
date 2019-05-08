d38f75111fb34d3f0022801c85a2868cc408f560
Nick Barnard <35875427+nbarn3@users.noreply.github.com>
2019-05-07 22:54:21 -0400

    Add files via upload
    Return view and coinciding views/updates

--- KSSPE/userinterface/CheckOutCollectionView.java
+++ KSSPE/userinterface/CheckOutCollectionView.java
@@ -48,20 +48,13 @@ import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.InnerShadow;
 import model.CheckOut;
 import model.CheckOutCollection;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 //==============================================================================
 public class CheckOutCollectionView extends View implements Observer
 {
 	protected TableView<CheckOutTableModel> tableOfCheckOuts;
 	protected Button cancelButton;
-	protected Button saveButton;
+	protected Button submitButton;
 	protected MessageView statusLog;
 	protected Text actionText; 
         
@@ -100,13 +93,15 @@ public class CheckOutCollectionView extends View implements Observer
 	//--------------------------------------------------------------------------
 	protected void getEntryTableModelValues()
 	{
+
 		ObservableList<CheckOutTableModel> tableData = FXCollections.observableArrayList();
 		try
 		{
-			CheckOutCollection checkOutCollection = 
+	
+			CheckOutCollection checkoutCollection = 
 					(CheckOutCollection)myController.getState("CheckOutList");
 
-			Vector entryList = (Vector)checkOutCollection.getState("CheckOuts");
+			Vector entryList = (Vector)checkoutCollection.getState("CheckOuts");
 			
 			if (entryList.size() > 0)
 			{
@@ -114,25 +109,25 @@ public class CheckOutCollectionView extends View implements Observer
 
 				while (entries.hasMoreElements() == true)
 				{
-					CheckOut nextC = (CheckOut)entries.nextElement();
+					CheckOut nextCO = (CheckOut)entries.nextElement();
 
-					Vector<String> view = nextC.getEntryListView();
+					Vector<String> view = nextCO.getEntryListView();
 
 					// add this list entry to the list
 					CheckOutTableModel nextTableRowData = new CheckOutTableModel(view);
 					tableData.add(nextTableRowData);
 
 				}
-
 				if(entryList.size() == 1)
-					actionText.setText(entryList.size()+" Record Found!");
-				else
-					actionText.setText(entryList.size()+" Records Found!");
+					actionText.setText(entryList.size()+" Reservation Found! Select to Return.");
+				else 
+					actionText.setText(entryList.size()+" Reservations Found! Select one to Return.");
+
 				actionText.setFill(Color.LIGHTGREEN);
 			}
 			else
 			{
-				actionText.setText("No Records Found!");
+				actionText.setText("No Reservations Found!");
 				actionText.setFill(Color.FIREBRICK);
 			}
 
@@ -140,6 +135,8 @@ public class CheckOutCollectionView extends View implements Observer
 		}
 		catch (Exception e) {//SQLException e) {
 			// Need to handle this exception
+			actionText.setText("ERROR: Unexpected error!");
+			actionText.setFill(Color.FIREBRICK);
 		}
 
 	}
@@ -183,9 +180,10 @@ public class CheckOutCollectionView extends View implements Observer
 		return container;
 	}
 	
+	//--------------------------------------------------------------
 	protected String getActionText()
 	{
-		return "** LIST OF RECORDS **";
+		return "** RESERVATIONS TO RETURN **";
 	}
 
 	// Create the main form content
@@ -199,45 +197,74 @@ public class CheckOutCollectionView extends View implements Observer
 		tableOfCheckOuts.setStyle("-fx-focus-color: transparent; -fx-faint-focus-color: transparent; -fx-selection-bar: gold;");
 		tableOfCheckOuts.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
-		TableColumn barcodeColumn = new TableColumn("Barcode") ;
-		barcodeColumn.setMinWidth(116.6);
-		barcodeColumn.setStyle(" -fx-alignment: CENTER;");
-		barcodeColumn.setCellValueFactory(
+		
+		TableColumn barcodePrefixColumn = new TableColumn("Barcode") ;
+		barcodePrefixColumn.setMinWidth(116.6);
+		barcodePrefixColumn.setStyle(" -fx-alignment: CENTER;");
+		barcodePrefixColumn.setCellValueFactory(
 				new PropertyValueFactory<CheckOutTableModel, String>("Barcode"));
 
-		TableColumn bannerIdColumn = new TableColumn("BannerId");
-		bannerIdColumn.setMinWidth(116.6);
-		bannerIdColumn.setStyle(" -fx-alignment: CENTER;");
-		bannerIdColumn.setCellValueFactory(
-				new PropertyValueFactory<CheckOutTableModel, String>("BannerId"));
 
-		TableColumn unitsTakenColumn = new TableColumn("Units Taken") ;
-		unitsTakenColumn.setMinWidth(116.6);
-		unitsTakenColumn.setStyle(" -fx-alignment: CENTER;");
-		unitsTakenColumn.setCellValueFactory(
+		TableColumn bannerColumn = new TableColumn("Banner ID") ;
+		bannerColumn.setMinWidth(116.6);
+		bannerColumn.setStyle(" -fx-alignment: CENTER;");
+		bannerColumn.setCellValueFactory(
+				new PropertyValueFactory<CheckOutTableModel, String>("BannerId"));
+		
+		TableColumn equipNameColumn = new TableColumn("Equipment Name") ;
+		equipNameColumn.setMinWidth(116.6);
+		equipNameColumn.setStyle(" -fx-alignment: CENTER;");
+		equipNameColumn.setCellValueFactory(
+				new PropertyValueFactory<CheckOutTableModel, String>("EquipmentName"));
+				
+		TableColumn takenColumn = new TableColumn("Units Taken") ;
+		takenColumn.setMinWidth(136.6);
+		takenColumn.setStyle(" -fx-alignment: CENTER;");
+		takenColumn.setCellValueFactory(
 				new PropertyValueFactory<CheckOutTableModel, String>("UnitsTaken"));
 
-		TableColumn unitsReturnedColumn = new TableColumn("Units Returned");
-		unitsReturnedColumn.setMinWidth(116.6);
-		unitsReturnedColumn.setStyle(" -fx-alignment: CENTER;");
-		unitsReturnedColumn.setCellValueFactory(
+		TableColumn returnedColumn = new TableColumn("Total Units Returned") ;
+		returnedColumn.setMinWidth(136.6);
+		returnedColumn.setStyle(" -fx-alignment: CENTER;");
+		returnedColumn.setCellValueFactory(
 				new PropertyValueFactory<CheckOutTableModel, String>("TotalUnitsReturned"));
 
-		TableColumn rentDateColumn = new TableColumn("Rented on");
-		rentDateColumn.setMinWidth(116.6);
-		rentDateColumn.setStyle(" -fx-alignment: CENTER;");
-		rentDateColumn.setCellValueFactory(
-				new PropertyValueFactory<CheckOutTableModel, String>("RentDate"));
-
-		TableColumn dueDateColumn = new TableColumn("Due on");
-		dueDateColumn.setMinWidth(116.6);
-		dueDateColumn.setStyle(" -fx-alignment: CENTER;");
-		dueDateColumn.setCellValueFactory(
+		TableColumn dueColumn = new TableColumn("Due Date") ;
+		dueColumn.setMinWidth(136.6);
+		dueColumn.setStyle(" -fx-alignment: CENTER;");
+		dueColumn.setCellValueFactory(
 				new PropertyValueFactory<CheckOutTableModel, String>("DueDate"));
 
-		tableOfCheckOuts.getColumns().addAll(barcodeColumn, bannerIdColumn, unitsTakenColumn, unitsReturnedColumn, rentDateColumn, dueDateColumn);
+		TableColumn rentColumn = new TableColumn("Check Out Date") ;
+		rentColumn.setMinWidth(136.6);
+		rentColumn.setStyle(" -fx-alignment: CENTER;");
+		rentColumn.setCellValueFactory(
+				new PropertyValueFactory<CheckOutTableModel, String>("RentDate"));
 
-		ImageView icon = new ImageView(new Image("/images/return.png"));
+		tableOfCheckOuts.getColumns().addAll(bannerColumn, barcodePrefixColumn, equipNameColumn, takenColumn, returnedColumn, dueColumn, rentColumn);
+
+		tableOfCheckOuts.setOnMousePressed((MouseEvent event) -> {
+			if (event.isPrimaryButtonDown() && event.getClickCount() >=2 ){
+				processCheckOutSelected();
+			}
+		});
+		ImageView icon = new ImageView(new Image("/images/check.png"));
+		icon.setFitHeight(15);
+		icon.setFitWidth(15);
+		submitButton = new Button("Select",icon);
+		submitButton.setFont(Font.font("Comic Sans", FontWeight.THIN, 14));
+		submitButton.requestFocus();
+		submitButton.setOnAction((ActionEvent e) -> {
+			clearErrorMessage();
+			processCheckOutSelected();
+		});
+		submitButton.addEventHandler(MouseEvent.MOUSE_ENTERED, (MouseEvent e) -> {
+			submitButton.setEffect(new DropShadow());
+		});
+		submitButton.addEventHandler(MouseEvent.MOUSE_EXITED, (MouseEvent e) -> {
+			submitButton.setEffect(null);
+		});
+		icon = new ImageView(new Image("/images/return.png"));
 		icon.setFitHeight(15);
 		icon.setFitWidth(15);
 		cancelButton = new Button("Return", icon);
@@ -261,41 +288,15 @@ public class CheckOutCollectionView extends View implements Observer
                 btnContainer.addEventHandler(MouseEvent.MOUSE_EXITED, (MouseEvent e) -> {
                     btnContainer.setStyle("-fx-background-color: SLATEGREY");
 		});
-
-        icon = new ImageView(new Image("/images/savecolor.png"));
-		icon.setFitHeight(15);
-		icon.setFitWidth(15);
-		saveButton = new Button("Save to File", icon);
-		saveButton.setGraphic(icon);
-                saveButton.setPadding(new Insets(5,5,5,5));
-		saveButton.setFont(Font.font("Comic Sans", FontWeight.THIN, 14));
-		saveButton.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent e) {
-				clearErrorMessage();
-                                saveToExcelFile();
-			}
-		});
-		saveButton.addEventHandler(MouseEvent.MOUSE_ENTERED, (MouseEvent e) -> {
-			saveButton.setEffect(new DropShadow());
-		});
-		saveButton.addEventHandler(MouseEvent.MOUSE_EXITED, (MouseEvent e) -> {
-			saveButton.setEffect(null);
-		});
-
-        btnContainer.addEventHandler(MouseEvent.MOUSE_ENTERED, (MouseEvent e) -> {
-            btnContainer.setStyle("-fx-background-color: GOLD");
-		});
-        btnContainer.addEventHandler(MouseEvent.MOUSE_EXITED, (MouseEvent e) -> {
-            btnContainer.setStyle("-fx-background-color: SLATEGREY");
-		});
-		btnContainer.setAlignment(Pos.CENTER);
-                btnContainer.getChildren().add(saveButton);
+		btnContainer.getChildren().add(submitButton);
 		btnContainer.getChildren().add(cancelButton);
 
 		tableOfCheckOuts.setPrefHeight(275);
-        tableOfCheckOuts.setMaxWidth(3000);
+        tableOfCheckOuts.setMaxWidth(550);
+		
+		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.setPrefSize(450, 150);
+		scrollPane.setContent(tableOfCheckOuts); // should we use this? (Probably not - FX tables come with their own scroll pane)
 		
 		vbox.getChildren().add(tableOfCheckOuts);
 		vbox.getChildren().add(btnContainer);
@@ -304,6 +305,36 @@ public class CheckOutCollectionView extends View implements Observer
 
 		return vbox;
 	}
+
+	//--------------------------------------------------------------------------
+	protected void processCheckOutSelected()
+	{
+		CheckOutTableModel selectedItem = tableOfCheckOuts.getSelectionModel().getSelectedItem();
+
+		if(selectedItem != null)
+		{
+			String reservationId = selectedItem.getId();
+
+			myController.stateChangeRequest("CheckOutSelected", reservationId);
+		}
+	}
+	
+    //---------------------------------------------------------------------------
+	// private void displayRemoveAlert(){
+	// 	clearErrorMessage();
+	// 	Alert alert = new Alert(Alert.AlertType.CONFIRMATION,"BannerId: "+tableOfBorrowers.getSelectionModel().getSelectedItem().getBannerId()
+	// 			+"\nFirstName: "+tableOfBorrowers.getSelectionModel().getSelectedItem().getFirstName()
+	// 			+"\nLastName: "+tableOfBorrowers.getSelectionModel().getSelectedItem().getLastName(), ButtonType.YES, ButtonType.NO);
+	// 	alert.setHeaderText(null);
+	// 	alert.setTitle("Remove Borrower");
+	// 	alert.setHeaderText("Are you sure want to remove this Borrower?");
+	// 	((Stage)alert.getDialogPane().getScene().getWindow()).getIcons().add(new Image("images/BPT_LOGO_All-In-One_Color.png"));
+	// 	alert.showAndWait();
+
+	// 	if (alert.getResult() == ButtonType.YES) {
+	// 		processBorrowerSelected();
+	// 	}
+	// }
        
 	//--------------------------------------------------------------------------
 	protected MessageView createStatusLog(String initialMessage)
@@ -338,84 +369,13 @@ public class CheckOutCollectionView extends View implements Observer
 	{
 		statusLog.displayMessage(message);
 	}
+	
+	//------------------------------------------------------------
 	public void displayErrorMessage(String message)
 	{
 		statusLog.displayErrorMessage(message);
 	}
 
-	protected void writeToFile(String fName)
-    {
-    	Vector allColumnNames = new Vector();
-
-        try
-        {
-    	    FileWriter outFile = new FileWriter(fName);
-            PrintWriter out = new PrintWriter(outFile);
-            CheckOutCollection checkOutCollection = (CheckOutCollection)myController.getState("CheckOutList");
-            Vector entryList = (Vector)checkOutCollection.getState("CheckOuts");
-
-            if ((entryList == null) || (entryList.size() == 0))
-                return;
-
-            allColumnNames.addElement("ID");
-            allColumnNames.addElement("BannerId");
-            allColumnNames.addElement("Barcode");
-            allColumnNames.addElement("UnitsTaken");
-            allColumnNames.addElement("TotalUnitsReturned");
-            allColumnNames.addElement("RentDate");
-            allColumnNames.addElement("DueDate");
-            allColumnNames.addElement("CheckOutWorkerID");
-
-            String line = "ID, BannerId, Barcode, UnitsTaken, TotalUnitsReturned, RentDate, DueDate, CheckOutWorkerID";
-
-            out.println(line);
-
-            for (int k = 0; k < entryList.size(); k++)
-            {
-                String valuesLine = "";
-                CheckOut nextC = (CheckOut)entryList.elementAt(k);
-                Vector<String> nextRow = nextC.getEntryListView();
-
-                for (int j = 0; j < allColumnNames.size()-1; j++)
-                {
-                    String nextValue = nextRow.elementAt(j);
-                        if(nextValue != null)
-                            valuesLine += nextValue + ", ";
-                }
-
-                out.println(valuesLine);
-            }
-
-            // Also print the shift count and filter type
-            out.println("\nTotal number of Reservation Records: " + entryList.size());
-
-            // Finally, print the time-stamp
-            DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
-            DateFormat timeFormat = new SimpleDateFormat("hh:mm aaa");
-            Date date = new Date();
-            String timeStamp = dateFormat.format(date) + " " +
-                    timeFormat.format(date);
-
-            out.println("Reservations Report created on " + timeStamp);
-
-            out.close();
-
-            // Acknowledge successful completion to user with JOptionPane
-            //JOptionPane.showMessageDialog(null, "Report data saved successfully to selected file");
-            }
-
-            catch (FileNotFoundException e)
-            {
-            //     JOptionPane.showMessageDialog(null, "Could not access file to save: "
-            //             + fName, "Save Error", JOptionPane.ERROR_MESSAGE );
-            }
-            catch (IOException e)
-            {
-            //     JOptionPane.showMessageDialog(null, "Error in saving to file: "
-            //             + e.toString(), "Save Error", JOptionPane.ERROR_MESSAGE );
-
-            }
-    }
 
 	/**
 	 * Clear error message
---
+++ KSSPE/userinterface/ReturnEquipmentView.java
@@ -0,0 +1,442 @@
+// specify the package
+package userinterface;
+
+// system imports
+import utilities.GlobalVariables;
+import utilities.Utilities;
+
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.collections.FXCollections;
+import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DatePicker;
+import javafx.scene.Scene;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
+import java.util.Properties;
+import java.util.Observer;
+import java.util.Observable;
+
+// project imports
+import java.util.Enumeration;
+import java.util.Vector;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ComboBox;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.util.StringConverter;
+
+import controller.Transaction;
+
+/** The class containing the Add Borrower View for the KSSPE
+ *  application 
+ */
+//==============================================================
+public class ReturnEquipmentView extends View implements Observer
+{
+
+	// GUI components
+	protected TextField barcode;
+	protected TextField equipName;
+	protected TextField count;
+	
+	protected DatePicker datePicker;
+	
+	protected GridPane grid;
+	protected Font myFont;
+
+	protected Text actionText;
+	protected Text prompt;
+	
+	protected HBox doneCont;
+	protected Button submitButton;
+	protected Button cancelButton;
+
+	protected MessageView statusLog;
+
+	// constructor for this class -- takes a controller object
+	//----------------------------------------------------------
+	public ReturnEquipmentView(Transaction t)
+	{
+		super(t);
+
+		VBox container = new VBox(10);
+		container.setStyle("-fx-background-color: slategrey");
+		container.setPadding(new Insets(15, 5, 5, 5));
+
+		container.getChildren().add(createTitle());
+
+		container.getChildren().add(createFormContent());
+
+		container.getChildren().add(createStatusLog("             "));
+
+		getChildren().add(container);
+		
+		populateFields();
+
+		myController.addObserver(this);
+		
+		
+	}
+
+	//-------------------------------------------------------------
+	protected String getActionText()
+	{
+		return "** RETURN EQUIPMENT **";
+	}
+	
+	//-----------------------------------------------------------------
+	public void populateFields()
+	{
+		barcode.setText((String)myController.getState("Barcode"));
+		equipName.setText((String)myController.getState("EquipmentName"));
+		count.setText((String)myController.getState("DefaultUnitsReturned"));
+		LocalDate today = LocalDate.now();
+		datePicker.setValue(today);
+
+		barcode.setDisable(true);
+		equipName.setDisable(true);
+		datePicker.setDisable(true);
+	}
+
+	//-----------------------------------------------------------------
+	protected void processBarcode(String Barcode)
+	{
+		//make sure equipment with this barcode actually exists. If so, inform the user and remove disables on the other fields.
+		Properties props = new Properties();
+		props.setProperty("Barcode", Barcode);
+		
+		
+	}
+
+	// Create the title container
+	//-------------------------------------------------------------
+	private Node createTitle()
+	{
+		VBox container = new VBox(10);
+		container.setPadding(new Insets(1, 10, 1, 10));
+		
+        Text clientText = new Text("KSSPE DEPARTMENT");
+			clientText.setFont(Font.font("Copperplate", FontWeight.EXTRA_BOLD, 36));
+			clientText.setEffect(new DropShadow());
+			clientText.setTextAlignment(TextAlignment.CENTER);
+			clientText.setFill(Color.WHITESMOKE);
+		container.getChildren().add(clientText);
+
+		Text titleText = new Text(" Reservation Management System ");
+			titleText.setFont(Font.font("Copperplate", FontWeight.THIN, 28));
+			titleText.setTextAlignment(TextAlignment.CENTER);
+			titleText.setFill(Color.GOLD);
+		container.getChildren().add(titleText);
+
+		Text blankText = new Text("  ");
+			blankText.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+			blankText.setWrappingWidth(350);
+			blankText.setTextAlignment(TextAlignment.CENTER);
+			blankText.setFill(Color.WHITE);
+		container.getChildren().add(blankText);
+
+		actionText = new Text("     " + getActionText() + "       ");
+			actionText.setFont(Font.font("Copperplate", FontWeight.BOLD, 22));
+			actionText.setWrappingWidth(450);
+			actionText.setTextAlignment(TextAlignment.CENTER);
+			actionText.setFill(Color.DARKGREEN);
+		container.getChildren().add(actionText);
+		
+		container.setAlignment(Pos.CENTER);
+
+		return container;
+	}
+
+	// Create the main form content
+	//-------------------------------------------------------------
+	private VBox createFormContent()
+	{
+		VBox vbox = new VBox(10);
+		vbox.setAlignment(Pos.CENTER);
+		
+		myFont = Font.font("Copperplate", FontWeight.THIN, 17);
+		Font bannerFont = Font.font("copperplate", FontWeight.BOLD, 17);   
+
+		Text blankText = new Text("  ");
+			blankText.setFont(Font.font("Arial", FontWeight.BOLD, 17));
+			blankText.setWrappingWidth(350);
+			blankText.setTextAlignment(TextAlignment.CENTER);
+			blankText.setFill(Color.WHITE);
+		vbox.getChildren().add(blankText);
+		
+		
+		
+		grid = new GridPane();
+			grid.setHgap(15);
+			grid.setVgap(15);
+			grid.setPadding(new Insets(0, 20, 20, 15));
+			grid.setAlignment(Pos.CENTER);
+		
+		Text barcodeLabel = new Text("Barcode :");
+			barcodeLabel.setFill(Color.GOLD);
+			barcodeLabel.setFont(bannerFont);
+			barcodeLabel.setUnderline(true);
+			barcodeLabel.setTextAlignment(TextAlignment.RIGHT);
+		grid.add(barcodeLabel, 0, 1);
+		
+		barcode = new TextField();
+			barcode.setMinWidth(110);
+			barcode.addEventFilter(KeyEvent.KEY_RELEASED, event->{
+				clearErrorMessage();
+
+			});
+		grid.add(barcode, 1, 1);
+		
+		Text equipNameLabel = new Text("Equipment Name:");
+			equipNameLabel.setFill(Color.GOLD);
+			equipNameLabel.setFont(myFont);
+			equipNameLabel.setTextAlignment(TextAlignment.RIGHT);
+		grid.add(equipNameLabel, 0, 2);
+		
+		equipName = new TextField();
+			equipName.setMinWidth(110);
+			equipName.setMinWidth(110);
+			equipName.addEventFilter(KeyEvent.KEY_RELEASED, event->{
+				clearErrorMessage();
+			});
+		grid.add(equipName, 1, 2);
+		
+		Text countLabel = new Text("Units Returned:");
+			countLabel.setFill(Color.GOLD);
+			countLabel.setFont(myFont);
+			countLabel.setTextAlignment(TextAlignment.RIGHT);
+		grid.add(countLabel, 0, 3);
+		
+		count = new TextField();
+			count.setMinWidth(110);
+			count.setMinWidth(110);
+			count.addEventFilter(KeyEvent.KEY_RELEASED, event->{
+				clearErrorMessage();
+			});
+		grid.add(count, 1, 3);
+		
+		Text dueLabel = new Text("Return Date :");
+			dueLabel.setFill(Color.GOLD);
+			dueLabel.setFont(myFont);
+			dueLabel.setTextAlignment(TextAlignment.RIGHT);
+		grid.add(dueLabel, 0, 4);
+		
+		
+		datePicker = new DatePicker();
+			datePicker.setMinWidth(110);
+			datePicker.setMinWidth(110);
+			datePicker.addEventFilter(KeyEvent.KEY_RELEASED, event->{
+				clearErrorMessage();
+			});
+		grid.add(datePicker, 1, 4);
+		
+		
+		//---------------------------------- middle grid done.
+		
+		
+		doneCont = new HBox(10);
+		doneCont.setAlignment(Pos.CENTER);
+            doneCont.addEventHandler(MouseEvent.MOUSE_ENTERED, (MouseEvent e) -> {
+            doneCont.setStyle("-fx-background-color: GOLD");
+		});
+        doneCont.addEventHandler(MouseEvent.MOUSE_EXITED, (MouseEvent e) -> {
+            doneCont.setStyle("-fx-background-color: SLATEGREY");
+		});
+		
+		ImageView icon = new ImageView(new Image("/images/buyingcolor.png"));
+			icon.setFitHeight(15);
+			icon.setFitWidth(15);
+			
+		submitButton = new Button("Return Equipment", icon);
+			submitButton.setFont(Font.font("Comic Sans", FontWeight.THIN, 14));
+			submitButton.setOnAction((ActionEvent e) -> {
+				sendToController();
+			});
+			submitButton.addEventHandler(MouseEvent.MOUSE_ENTERED, (MouseEvent e) -> {
+				submitButton.setEffect(new DropShadow());
+			});
+			submitButton.addEventHandler(MouseEvent.MOUSE_EXITED, (MouseEvent e) -> {
+				submitButton.setEffect(null);
+			});
+		doneCont.getChildren().add(submitButton);
+		
+		icon = new ImageView(new Image("/images/return.png"));
+			icon.setFitHeight(15);
+			icon.setFitWidth(15);
+			
+		cancelButton = new Button("Return", icon);
+			cancelButton.setFont(Font.font("Comic Sans", FontWeight.THIN, 14));
+			cancelButton.setOnAction((ActionEvent e) -> {
+				clearErrorMessage();
+			
+				myController.stateChangeRequest("CancelTransaction", null);
+			});
+			cancelButton.addEventHandler(MouseEvent.MOUSE_ENTERED, (MouseEvent e) -> {
+				cancelButton.setEffect(new DropShadow());
+			});
+			cancelButton.addEventHandler(MouseEvent.MOUSE_EXITED, (MouseEvent e) -> {
+				cancelButton.setEffect(null);
+			});
+		doneCont.getChildren().add(cancelButton);
+		
+		vbox.getChildren().add(grid);
+		vbox.getChildren().add(doneCont);
+		
+	
+		setOutlines();
+		
+		return vbox;
+	}
+
+	//---------------------------------------------------------------------------------------------------
+	protected void sendToController()
+	{
+		clearErrorMessage();
+		
+		String Barcode = barcode.getText();
+		String Count = count.getText();
+		String ReturnDate = (datePicker.getValue()).toString();
+		
+		if(Utilities.checkBarcode(Barcode))
+		{
+			if(Utilities.checkGoodCount(Count))
+			{
+				if(Utilities.checkReturnDate(ReturnDate))
+				{
+					Properties props = new Properties();
+					props.setProperty("Barcode", Barcode);
+					props.setProperty("UnitsReturned", Count);
+					props.setProperty("ReturnDate", ReturnDate);
+					myController.stateChangeRequest("CheckInData", props);
+				}
+				else
+				{
+					displayErrorMessage("Please enter a valid date in the form yyyy-mm-dd");
+					datePicker.requestFocus();
+				}
+			}
+			else
+			{
+				displayErrorMessage("Please enter a valid count.");
+				count.requestFocus();
+			}
+		}
+		else
+		{
+			displayErrorMessage("Please enter a valid barcode.");
+			barcode.requestFocus();
+		}
+	}
+	
+	//-------------------------------------------------------------
+	protected MessageView createStatusLog(String initialMessage)
+	{
+		statusLog = new MessageView(initialMessage);
+
+		return statusLog;
+	}
+
+	//-------------------------------------------------------------
+	public void clearValues()
+	{
+		barcode.clear();
+		count.clear();
+	}
+	
+	//------------------------------------------------------------------------------------------
+	private void removeDisables()
+	{
+		count.setDisable(false);
+		datePicker.setDisable(false);
+	}
+	
+	//------------------------------------------------------------------------------------------
+	protected void setDisables()
+	{
+		count.setDisable(true);
+		datePicker.setDisable(true);
+	}
+	
+	
+	//------------------------------------------------------------------------------------------
+	private void setOutlines()
+	{
+		barcode.setStyle("-fx-border-color: transparent; -fx-focus-color: green;");
+		count.setStyle("-fx-border-color: transparent; -fx-focus-color: green;");
+		datePicker.setStyle("-fx-border-color: transparent; -fx-focus-color: green;");
+	}
+
+	/**
+	 * Update method
+	 */
+	//---------------------------------------------------------
+	
+	public void update(Observable o, Object value)
+	{
+		clearErrorMessage();
+
+		String val = (String)value;
+		if (val.startsWith("ERR") == true)
+		{
+			displayErrorMessage(val);
+		}
+		else
+		{
+			
+			displayMessage(val);
+		}
+		
+	}
+
+	/**
+	 * Display error message
+	 */
+	//----------------------------------------------------------
+	public void displayErrorMessage(String message)
+	{
+		statusLog.displayErrorMessage(message);
+	}
+
+	/**
+	 * Display info message
+	 */
+	//----------------------------------------------------------
+	public void displayMessage(String message)
+	{
+		statusLog.displayMessage(message);
+	}
+
+	/**
+	 * Clear error message
+	 */
+	//----------------------------------------------------------
+	public void clearErrorMessage()
+	{
+		statusLog.clearErrorMessage();
+	}
+
+}
+
+//---------------------------------------------------------------
+//	Revision History:
+//
